61043722caf8d245e42915c1ca7e27b9e81e50bb
Юрий Кузнецов <omega131995@outlook.com>
2024-07-30 06:35:37 +0600

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+      react(),
+      federation({
+        name: "catalog-components",
+        filename: "remoteEntry.js",
+        exposes: [
+
+        ],
+        shared: [
+            "react"
+        ]
+      })
+  ],
+})
